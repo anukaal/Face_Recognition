f6e2a0401484043b53e7593e1282498becb8e151
anukaal <mailanu98@gmail.com>
2020-10-20 06:45:30 +0530

    importing req

---
+++ face_recog.py
@@ -0,0 +1,12 @@
+# to regonise face 
+
+import cv2
+import numpy as np
+import os 
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')   #load trained model
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+
+font = cv2.FONT_HERSHEY_SIMPLEX
